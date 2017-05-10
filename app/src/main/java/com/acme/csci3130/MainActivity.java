6f75acb793b53d96dca0a7a5688126411ba9123d
Michael <m.altair@live.com>
2017-05-10 15:46:54 -0300

    The update button can now change the text view with what has been entered by the user

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // This gets called when the user presses the update button
+    public void sendMessage(View view) {
+        // Grabbing the components
+        TextView textView = (TextView) findViewById(R.id.helloText);
+        EditText newText = (EditText) findViewById(R.id.textInput);
+
+        // Updating the values
+        textView.setText(newText.getText());
+        newText.setText(""); // Clearing what the user originally entered
     }
 }
